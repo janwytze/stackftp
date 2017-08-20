83de528409ffc87d4704ca4bbd4ccd7113ef21ab
Janyk Steenbeek <info@janyksteenbeek.nl>
2017-08-21 00:13:54 +0200

    Undo temporary maven fix

--- src/main/java/nl/stackftp/StackftpApplication.java
+++ src/main/java/nl/stackftp/StackftpApplication.java
@@ -1,13 +1,9 @@
 package nl.stackftp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-
 public class StackftpApplication {
 
 	public static void main(String[] args) {
