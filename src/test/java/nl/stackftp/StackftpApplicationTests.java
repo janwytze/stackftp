74c428ba4ad2519af33a576b4719879c63b012ef
Jan Wytze <jwzuid@live.nl>
2017-08-20 20:09:04 +0200

    Inital commit

---
+++ src/test/java/nl/stackftp/StackftpApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.stackftp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StackftpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
